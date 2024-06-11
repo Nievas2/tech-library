1036ba7f286d75d514ce45d62562058e64ddee90
Nievas2 <angelgabrielnievas@gmail.com>
2024-06-11 17:09:08 -0300

    style(front): :lipstick: add font

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -72,6 +72,9 @@ module.exports = {
         "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
+    fontFamily: {
+      poppins : ["Poppins", "sans-serif"],
+    }
   },
   plugins: [require("tailwindcss-animate")],
 }
