e41781aa61200b71034dbcad2725605a73cecf7d
Nievas2 <angelgabrielnievas@gmail.com>
2024-06-12 15:29:44 -0300

    feat(front): :sparkles: add custom colors in tailwind

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,11 +1,14 @@
-
+import Navbar from "./components/shared/Navbar"
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </>
+    <main className="bg-light dark:bg-dark">
+      <Navbar />
+      <section className="h-[100vh] w-full">
+        <h1 className="text-3xl font-bold underline ">Hello world!</h1>
+      </section>
+      
+    </main>
+      
   )
 }
 
