8eb6c4b4eee6f66680d61e89415eb78fcea6b71b
SaniEHerr <santiagoeherrera@gmail.com>
2024-06-13 19:01:28 -0300

    feat(front): :sparkles: Add Cards

--- front/src/pages/home/HomePage.tsx
+++ front/src/pages/home/HomePage.tsx
@@ -1,8 +1,29 @@
+import Card from "@/components/shared/Card"
 
 const HomePage = () => {
   return (
     <>
       <h1>SerachBar</h1>
+      <div className="mx-auto max-w-[1240px] grid sm:grid-cols-2 lg:grid-cols-3 justify-center gap-10 mb-10">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
     </>
   )
 }
