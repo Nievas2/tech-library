edfb0a2644bb72e50daa6e39f6c0c0a2cf854d36
Nievas2 <angelgabrielnievas@gmail.com>
2024-06-17 14:41:45 -0300

    fix(front): :bug: persist working

--- front/src/components/shared/Card.tsx
+++ front/src/components/shared/Card.tsx
@@ -1,31 +1,48 @@
 import { Heart, Link } from "lucide-react"
 import { Button } from "../ui/button"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { NavLink } from "react-router-dom"
 import { Library } from "@/interfaces/Library"
 import { Tag } from "@/interfaces/Tag"
+import { useLibraries } from "@/stores/Library"
 
 interface CardProps {
   card: Library
 }
-const Card = ({ card  }: CardProps) => {
-  const [favorite, setFavorite] = useState(false)
+const Card = ({ card }: CardProps) => {
+  const [favoriteActive, setFavoriteActive] = useState(false)
+  const addFavoriteLibrary = useLibraries((state) => state.addFavoriteLibrary)
+  const deleteFavoriteLibrary = useLibraries(
+    (state) => state.deleteFavoriteLibrary
+  )
+  const favorites = useLibraries((state) => state.favorites)
 
+  function handleFavorite() {
+    if (!favoriteActive) {
+      setFavoriteActive(!favoriteActive)
+      addFavoriteLibrary(card)
+    } else {
+      setFavoriteActive(!favoriteActive)
+      const index = favorites?.findIndex((favorite) => favorite.id == card.id)
+      if (index >= 0 && index != undefined) deleteFavoriteLibrary(index)
+    }
+  }
+  useEffect(() => {
+    favorites?.find((favorite) => favorite.id === card.id)
+      ? setFavoriteActive(true)
+      : setFavoriteActive(false)
+  }, [])
   return (
     <div className="flex bg-main/15 flex-col justify-between gap-6 border border-dark dark:border-light rounded-md shadow-xl hover:scale-[1.03] hover:transition-transform duration-500 p-4">
       <div className="flex flex-col gap-2">
-        <h2 className="text-2xl font-bold">
-          {card.name}
-        </h2>
+        <h2 className="text-2xl font-bold">{card.name}</h2>
 
-        <p className="text-base">
-          {card.description}
-        </p>
+        <p className="text-base">{card.description}</p>
 
         <div className="flex flex-row flex-wrap gap-2 text-sm">
           {card.tags?.map((tag: Tag) => {
             // const tagColor = tagColors[tag] || { bgColor: '#ffffff45', borderColor: '#000000' };
-            const tagColor = tag || { bgColor: '#ffffff45' };
+            const tagColor = tag || { bgColor: "#ffffff45" }
             return (
               <h4
                 key={crypto.randomUUID()}
@@ -33,27 +50,37 @@ const Card = ({ card  }: CardProps) => {
                 // style={{ backgroundColor: tagColor.bgColor, borderColor: tagColor.borderColor }}
                 // className="px-2 py-1 rounded-lg border font-extrabold text-dark [text-shadow:_1px_1px_0px_rgb(255_255_255_/_100%)] dark:text-light dark:[text-shadow:_1px_1px_0_rgb(0_0_0_/_100%)]">
                 // border-dark dark:border-light
-                className="px-2 py-1 rounded-lg font-extrabold text-stroke-dark dark:text-stroke-light"> 
+                className="px-2 py-1 rounded-lg font-extrabold text-stroke-dark dark:text-stroke-light"
+              >
                 {tag.name}
               </h4>
-            );
+            )
           })}
         </div>
       </div>
 
       <div className="flex flex-col gap-4">
         <div className="flex flex-row gap-4 justify-center items-center">
-          <NavLink className="flex flex-grow" to="/favorites">
-            <Button variant="directLink" className="flex-grow flex flex-row gap-2 justify-center items-center cursor-pointe">
+          <NavLink
+            className="flex flex-grow"
+            to="/favorites"
+          >
+            <Button
+              variant="directLink"
+              className="flex-grow flex flex-row gap-2 justify-center items-center cursor-pointe"
+            >
               <Link />
               <p>Direct Link</p>
             </Button>
           </NavLink>
 
-          <div onClick={() => setFavorite(!favorite)}>
-            {favorite ? (
+          <div onClick={handleFavorite}>
+            {favoriteActive ? (
               <div className="animate-fade-in-scale mx-auto flex justify-center items-center cursor-pointer border border-[#E81224] px-4 py-2 rounded-lg bg-[#E81224]/35">
-                <Heart className="text-[#E81224]" fill="#E81224" />
+                <Heart
+                  className="text-[#E81224]"
+                  fill="#E81224"
+                />
               </div>
             ) : (
               <div className="mx-auto flex justify-center items-center cursor-pointer border border-dark dark:border-light px-4 py-2 rounded-lg hover:bg-[#E81224]/20  transition-colors duration-300">
@@ -64,7 +91,12 @@ const Card = ({ card  }: CardProps) => {
         </div>
 
         <div className="flex flex-row items-center justify-end gap-2">
-          <small>Suggested by <span className="font-semibold text-main">{card.createdBy.name}</span></small>
+          <small>
+            Suggested by{" "}
+            <span className="font-semibold text-main">
+              {card.createdBy.name}
+            </span>
+          </small>
           <small>|</small>
           <small>02-08-1999</small>
         </div>
--- front/src/components/shared/CardsContainer.tsx
+++ front/src/components/shared/CardsContainer.tsx
@@ -5,6 +5,7 @@ interface CardsContainerProps {
   cards: Library[]
 }
 const CardsContainer = ({ cards }: CardsContainerProps) => {
+  console.log("render")
   return (
     <div className="mx-auto max-w-[1240px] grid sm:grid-cols-2 lg:grid-cols-3 justify-center gap-5 mb-10">
       {cards.map((card: Library, index: number) => (
