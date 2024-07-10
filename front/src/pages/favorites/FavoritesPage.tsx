e26ba9fdb1ec215bd7de1594bdafec43c63b583f
SaniEHerr <santiagoeherrera@gmail.com>
2024-07-10 14:00:01 -0300

    fix(front): :bug: favorites funcionality

--- front/src/pages/favorites/FavoritesPage.tsx
+++ front/src/pages/favorites/FavoritesPage.tsx
@@ -3,8 +3,19 @@ import { useFavoriteStore } from "@/stores";
 
 const FavoritesPage = () => {
   const favorites = useFavoriteStore((state) => state.favorites)
-  
-  return <div>{favorites && <CardsContainer libraries={favorites} />}</div>
+
+  return (
+    <div>
+      {favorites.length > 0 ? (
+        <CardsContainer libraries={favorites} />
+      ) : (
+        <div className="flex justify-center items-center h-[65vh] text-center">
+          <p className="text-2xl font-bold">You haven't added anything to your favorites yet...</p>
+        </div>
+      )}
+    </div>
+  )
 }
+
 
 export default FavoritesPage
