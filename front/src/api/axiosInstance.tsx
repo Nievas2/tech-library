4b8fcbf490af702497fb3b16e95c95dbc90f5df5
Nievas2 <angelgabrielnievas@gmail.com>
2024-07-10 15:02:46 -0300

    fix(front): :bug: Change api url

--- front/src/api/axiosInstance.tsx
+++ front/src/api/axiosInstance.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
 
