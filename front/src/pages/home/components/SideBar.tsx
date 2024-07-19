3f43d6d588567bfdf936a81e0df1439945936634
Nievas2 <angelgabrielnievas@gmail.com>
2024-07-19 15:56:36 -0300

    feat(front): :sparkles: Add searchbar tags

--- front/src/pages/home/components/SideBar.tsx
+++ front/src/pages/home/components/SideBar.tsx
@@ -5,12 +5,17 @@ import { Button } from "@/components/ui/button"
 import { getTagsApi } from "@/services/TagService"
 import { Tag, useTagStore } from "@/stores"
 import usePaginationHome from "@/hooks/usePaginationHome"
+import { Input } from "@/components/ui/input"
+import { useDebounce } from "use-debounce"
 
 export default function SideBar() {
   const [open, setOpen] = useState(window.innerWidth > 768)
   const setTags = useTagStore((state) => state.setTags)
   const tags = useTagStore((state) => state.tags)
+  //const [tagsSearch, setTagsSearch] = useState<Tag[] | undefined>()
   const [loading, setLoading] = useState(true)
+  const [text, setText] = useState("")
+  const [value] = useDebounce(text, 350)
   const { searchParams } = usePaginationHome()
 
   useEffect(() => {
@@ -24,31 +29,41 @@ export default function SideBar() {
       window.removeEventListener("resize", handleResize)
     }
   }, [])
-
+  const fetchTags = async () => {
+    try {
+      const response = await getTagsApi()
+      const tagsIdsParams = String(searchParams.get("tags"))
+      const ids = tagsIdsParams.split(",").map(Number)
+      const updatedObjectsArray = response.map((obj) => {
+        return {
+          ...obj,
+          selected: ids.includes(Number(obj.id))
+        }
+      })
+      setTags(updatedObjectsArray)
+    } catch (error) {
+      console.error("Error fetching tags", error)
+    } finally {
+      setLoading(false)
+    }
+  }
   useEffect(() => {
     setLoading(true)
-    const fetchTags = async () => {
-      try {
-        const response = await getTagsApi()
-        const tagsIdsParams = String(searchParams.get("tags"))
-        const ids = tagsIdsParams.split(",").map(Number)
-        const updatedObjectsArray = response.map((obj) => {
-          return {
-            ...obj,
-            selected: ids.includes(Number(obj.id))
-          }
-        })
-        setTags(updatedObjectsArray)
-      } catch (error) {
-        console.error("Error fetching tags", error)
-      } finally {
-        setLoading(false)
-      }
+    handleChangeSelect(value)
+  }, [value])
+  function handleChangeSelect(value: string) {
+    if (value.length === 0) {
+      fetchTags()
+    } else {
+      const tagsFiltered = tags.filter((tag: Tag) => {
+        if (tag.name.toLowerCase().includes(value.toLowerCase())) {
+          return tag
+        }
+      })
+      setTags(tagsFiltered)
     }
-
-    fetchTags()
-  }, [])
-
+    setLoading(false)
+  }
   const handleSidebar = () => {
     setOpen(!open)
   }
@@ -90,18 +105,58 @@ export default function SideBar() {
             }`}
           >
             <h2 className="text-xl font-bold">CATEGORIES</h2>
+            <div className="w-full flex h-8 border border-dark dark:border-light rounded-md">
+              <div className="w-full relative rounded-md">
+                <Input
+                  placeholder="Search tag"
+                  value={text}
+                  onChange={(e) => {
+                    setText(e.target.value)
+                  }}
+                  className="w-full bg-light focus:ring-0 disabled:focus:ring text-dark dark:text-light dark:bg-dark rounded-l-none h-full rounded-md"
+                />
+
+                {text && (
+                  <button
+                    className="absolute right-2 top-1/2 -translate-y-1/2 z-0"
+                    onClick={() => setText("")}
+                  >
+                    <Icon
+                      icon="material-symbols:close"
+                      width="24"
+                      height="24"
+                    />
+                  </button>
+                )}
+              </div>
+            </div>
             <ul className="flex flex-col gap-1 overflow-y-scroll scroll-smooth h-[320px]">
               {loading ? (
                 <p>Loading...</p>
               ) : (
-                <div className="flex flex-wrap gap-3">
-                  {tags &&
-                    tags?.map((tag: Tag) => (
-                      <ItemsSideBar
-                        key={tag.name}
-                        tag={tag}
-                      />
-                    ))}
+                <div>
+                  {
+                    tags && (
+                      <div className="flex flex-wrap gap-3">
+                        {tags?.map((tag: Tag) => (
+                          <ItemsSideBar
+                            key={crypto.randomUUID()}
+                            tag={tag}
+                          />
+                        ))}
+                      </div>
+                    ) /* : (
+                    <div className="flex flex-wrap gap-3">
+                      {tags &&
+                        tags?.map((tag: Tag) => (
+                          <ItemsSideBar
+                            key={crypto.randomUUID()}
+                            tag={tag}
+                          />
+                        ))}
+                    </div>
+                  ) */
+                  }
                 </div>
               )}
             </ul>
