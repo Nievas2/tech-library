a3d00b7214586f1c860b6711412034c691081d74
Nievas2 <angelgabrielnievas@gmail.com>
2024-07-19 12:56:35 -0300

    fix(front): :lipstick: Padding in favorites

--- front/src/pages/favorites/FavoritesPage.tsx
+++ front/src/pages/favorites/FavoritesPage.tsx
@@ -5,12 +5,12 @@ const FavoritesPage = () => {
   const favorites = useFavoriteStore((state) => state.favorites)
 
   return (
-    <div className="pb-4">
+    <div className="p-4 pt-0">
       <h1 className="text-3xl font-bold mb-4 text-center">Favorites</h1>
       {favorites.length > 0 ? (
         <CardsContainer libraries={favorites} />
       ) : (
-        <div className="flex justify-center items-center h-[65vh] text-center">
+        <div className="flex justify-center items-center h-[58vh] text-center">
           <p className="text-2xl font-bold">You haven't added anything to your favorites yet...</p>
         </div>
       )}
